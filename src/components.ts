77552a527ee7e6a863a5db67714dfe9f405599d4
Niko Salminen <niko@salminen.me>
2020-09-08 20:55:22 +0300

    Initial commit

---
+++ src/components.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@kaivosukeltaja/niko-ds';
+
+
+
+export const MyButton = /*@__PURE__*/createReactComponent<JSX.MyButton, HTMLMyButtonElement>('my-button');
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
+export const MyIcon = /*@__PURE__*/createReactComponent<JSX.MyIcon, HTMLMyIconElement>('my-icon');
